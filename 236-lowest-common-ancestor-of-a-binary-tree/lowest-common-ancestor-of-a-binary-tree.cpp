7fdf864d24b3bfd250d6cf56fd693a505e103606
Aniket Guleria <aniketguleria057@gmail.com>
2026-07-30 11:25:58 +0530

    Time: 64 ms (50.62%) | Memory: 44 MB (24.66%) - LeetSync

--- 236-lowest-common-ancestor-of-a-binary-tree/lowest-common-ancestor-of-a-binary-tree.cpp
+++ 236-lowest-common-ancestor-of-a-binary-tree/lowest-common-ancestor-of-a-binary-tree.cpp
@@ -9,32 +9,14 @@
  */
 class Solution {
 public:
-    bool root_to_leaf_path(TreeNode* root, TreeNode* x, vector<TreeNode*> &ans) {
-        if(root == nullptr) return false;
-        ans.push_back(root);
-        if(root == x) return true;
-
-        if(root_to_leaf_path(root->left, x, ans) || root_to_leaf_path(root->right, x, ans)) return true;
-
-        ans.pop_back();
-        return false;
-    }
     TreeNode* lowestCommonAncestor(TreeNode* root, TreeNode* p, TreeNode* q) {
-        if(root == nullptr) return root;
-        vector<TreeNode*> p1;
-        vector<TreeNode*> p2;
+        if(root == NULL || root == p || root == q) return root;
 
-        root_to_leaf_path(root, p, p1);
-        root_to_leaf_path(root, q, p2);
+        TreeNode* left = lowestCommonAncestor(root->left, p, q);
+        TreeNode* right = lowestCommonAncestor(root->right, p, q);
 
-        reverse(p1.begin(), p1.end());
-        reverse(p2.begin(), p2.end());
-
-        for(auto it1 : p1) {
-            for(auto it2 : p2) {
-                if(it1 == it2) return it1;
-            }
-        }
-        return nullptr;
+        if(left == NULL) return right;
+        else if(right == NULL) return left;
+        else return root;
     }
 };
